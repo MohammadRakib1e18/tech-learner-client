2fd9d25a1d2ca77cbebfbdcc89222c953976641d
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-10-27 20:04:41 +0600

    site deployed

--- src/Pages/Shared/SideBar/SideBar.js
+++ src/Pages/Shared/SideBar/SideBar.js
@@ -5,7 +5,7 @@ const SideBar = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/categories")
+    fetch("https://tech-learner-server.vercel.app/categories")
       .then((res) => res.json())
       .then((data) => setCategories(data));
   }, []);
--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -75,19 +75,23 @@ export const routes = createBrowserRouter([
       {
         path: "/courses",
         element: <Courses></Courses>,
-        loader: () => fetch("http://localhost:5000/courses"),
+        loader: () => fetch("https://tech-learner-server.vercel.app/courses"),
       },
       {
         path: "/courses/category/:id",
         element: <Category></Category>,
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/courses/category/${params.id}`),
+          fetch(
+            `https://tech-learner-server.vercel.app/courses/category/${params.id}`
+          ),
       },
       {
         path: "/courses/course/:id",
         element: <CourseDetails></CourseDetails>,
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/courses/course/${params.id}`),
+          fetch(
+            `https://tech-learner-server.vercel.app/courses/course/${params.id}`
+          ),
       },
       {
         path: "/courses/premium",
